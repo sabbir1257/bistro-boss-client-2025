2ef9f250a63db06360e0c43bedcd814bec9f1201
sabbir1257 <masabbir1257@gmail.com>
2025-03-09 00:12:34 +0600

    Create simple Top Banner react responsive carousel

--- src/pages/Shared/Footer/Footer.jsx
+++ src/pages/Shared/Footer/Footer.jsx
@@ -4,7 +4,7 @@ import { FaFacebookF, FaInstagram, FaTwitter } from "react-icons/fa";
 const Footer = () => {
   return (
     <footer>
-      <div className="md:grid grid-cols-2 justify-between mx-auto text-center ">
+      <div className="justify-between grid-cols-2 mx-auto text-center md:grid ">
         <div className="bg-[#1F2937] text-white py-16">
           <h2 className="mb-5 text-[32px] font-medium ">CONTACT US</h2>
           <p className="font-medium text-[20px]">
@@ -19,14 +19,14 @@ const Footer = () => {
           <p className="font-medium text-[20px] mb-5">
             Join us on social media
           </p>
-          <div className="flex gap-6 justify-center text-3xl">
+          <div className="flex justify-center gap-6 text-3xl">
             <FaFacebookF />
             <FaInstagram />
             <FaTwitter />
           </div>
         </div>
       </div>
-      <span class="block py-2 text-white sm:text-center bg-black ">
+      <span className="block py-2 text-white bg-black sm:text-center ">
         Copyright © CulinaryCloud. All rights reserved.
       </span>
     </footer>
