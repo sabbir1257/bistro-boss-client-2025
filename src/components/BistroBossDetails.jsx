0e1f17731a95e696a2f4e31242d119b0a6a00f90
sabbir1257 <masabbir1257@gmail.com>
2025-03-09 13:08:49 +0600

    all code push

---
+++ src/components/BistroBossDetails.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const BistroBossDetails = () => {
+  return (
+    <div class="bg-[url('../../src/assets/home/chef-service.jpg')] bg-cover bg-center h-[572px] w-[1280px] flex flex-col justify-center items-center text-center">
+      <div class="bg-white p-8 rounded-lg w-[920px] h-[333px] grid items-center">
+        <div>
+          <h1 class="text-4xl font-bold text-gray-800 mb-4">BISTRO BOSS</h1>
+          <p class=" mb-8">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Necessitatibus, libero accusamus laborum deserunt ratiene dolor
+            efficis praesentium! Deserunt magni aperiam dolor eius dolore at,
+            nihil luscio duciurus incidunt quibusdam nemo.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default BistroBossDetails;
