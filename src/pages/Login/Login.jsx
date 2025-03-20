39da63b419931222cd393457a709f7d9c1296723
sabbir1257 <masabbir1257@gmail.com>
2025-03-20 12:27:58 +0600

    Use Regex Validation In React Hook Form And Signup

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -6,6 +6,7 @@ import {
   validateCaptcha,
 } from "react-simple-captcha";
 import { AuthContext } from "../../providers/AuthProvider";
+import { Helmet } from "react-helmet-async";
 
 const Login = () => {
   const captchaRef = useRef(null);
@@ -41,87 +42,95 @@ const Login = () => {
   };
 
   return (
-    <section className="bg-gray-50 dark:bg-gray-900">
-      <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-        <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
-          <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-              Sign in to your account
-            </h1>
-            <form
-              onSubmit={handleSubmit}
-              className="space-y-4 md:space-y-6"
-              action="#"
-            >
-              <div>
-                <label
-                  htmlFor="email"
-                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                >
-                  Your email
-                </label>
+    <>
+      <Helmet>
+        <title>DIstro Boss | LogIn</title>
+      </Helmet>
+      <section className="bg-gray-50 dark:bg-gray-900">
+        <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+          <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+            <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+              <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                Sign in to your account
+              </h1>
+              <form
+                onSubmit={handleSubmit}
+                className="space-y-4 md:space-y-6"
+                action="#"
+              >
+                <div>
+                  <label
+                    htmlFor="email"
+                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  >
+                    Your email
+                  </label>
+                  <input
+                    type="email"
+                    name="email"
+                    id="email"
+                    className="bg-gray-50 border rounded-lg  block w-full p-2.5"
+                    placeholder="name@company.com"
+                    required
+                  />
+                </div>
+                <div>
+                  <label
+                    htmlFor="password"
+                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  >
+                    Password
+                  </label>
+                  <input
+                    type="password"
+                    name="password"
+                    id="password"
+                    placeholder="••••••••"
+                    className="bg-gray-50 border rounded-lg  block w-full p-2.5"
+                    required
+                  />
+                </div>
+                <div>
+                  <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                    <LoadCanvasTemplate />
+                  </label>
+                  <input
+                    type="text"
+                    name="captcha"
+                    ref={captchaRef}
+                    placeholder="type the text above"
+                    className="bg-gray-50 border rounded-lg  block w-full p-2.5"
+                    required
+                  />
+                  <button
+                    type="button" // Fix this: It should be type="button", not nClick
+                    onClick={handleValidateCaptcha}
+                    className="px-3 py-2 text-xs font-medium text-center text-white bg-[#D1A054]rounded-lg bg-[#D1A054] rounded-lg mt-2"
+                  >
+                    Validate Captcha
+                  </button>
+                </div>
                 <input
-                  type="email"
-                  name="email"
-                  id="email"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                  placeholder="name@company.com"
-                  required
+                  disabled={disabled}
+                  type="submit"
+                  value="Login"
+                  className="bg-[#D1A054] text-white p-3 rounded-lg mx-auto grid items-center"
                 />
-              </div>
-              <div>
-                <label
-                  htmlFor="password"
-                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                >
-                  Password
-                </label>
-                <input
-                  type="password"
-                  name="password"
-                  id="password"
-                  placeholder="••••••••"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                  required
-                />
-              </div>
-              <div>
-                <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-                  <LoadCanvasTemplate />
-                </label>
-                <input
-                  type="text"
-                  name="captcha"
-                  ref={captchaRef}
-                  placeholder="type the text above"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                  required
-                />
-                <button
-                  type="button" // Fix this: It should be type="button", not nClick
-                  onClick={handleValidateCaptcha}
-                  className="px-3 py-2 text-xs font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                >
-                  Validate Captcha
-                </button>
-              </div>
-              <input
-                disabled={disabled}
-                type="submit"
-                value="Login"
-                className="bg-[#D1A054] p-3 rounded-lg mx-auto grid items-center"
-              />
-              <p className="text-sm font-light text-[#D1A054]">
-                New here?{" "}
-                <Link to='/signUp' className="font-semibold text-primary-600 hover:underline ">
-                  Create a New Account
-                </Link>
-              </p>
-            </form>
+                <p className="text-sm font-light text-[#D1A054]">
+                  New here?{" "}
+                  <Link
+                    to="/signUp"
+                    className="font-semibold text-primary-600 hover:underline "
+                  >
+                    Create a New Account
+                  </Link>
+                </p>
+              </form>
+            </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </>
   );
 };
 
--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -1,6 +1,8 @@
+import { useContext } from "react";
+import { Helmet } from "react-helmet-async";
 import { useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
-
+import { AuthContext } from "../../providers/AuthProvider";
 const SignUp = () => {
   const {
     register,
@@ -8,98 +10,124 @@ const SignUp = () => {
     formState: { errors },
   } = useForm();
 
+const {createUser} = useContext(AuthContext)
+
   const onSubmit = (data) => {
     console.log(data);
+    createUser(data.email, data.password)
+    .then(result => {
+        const loggedUser = result.user;
+        console.log(loggedUser);
+    })
   };
 
-  /*   const handleSubmit = (e) => {
-    e.preventDefault();
-    const form = e.target;
-    const email = form.email.value;
-    const password = form.password.value;
-    console.log(email, password);
-  }; */
 
+  
   return (
-    <section className="bg-gray-50 dark:bg-gray-900">
-      <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-        <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
-          <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-              Sign Up to your account
-            </h1>
-            <form
-              onSubmit={handleSubmit(onSubmit)}
-              className="space-y-4 md:space-y-6"
-              action="#"
-            >
-              <div>
-                <label
-                  htmlFor="name"
-                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                >
-                  Your Name
-                </label>
+    <>
+      <Helmet>
+        <title>DIstro Boss | Sign Up</title>
+      </Helmet>
+      <section className="bg-gray-50 dark:bg-gray-900">
+        <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+          <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+            <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+              <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                Sign Up to your account
+              </h1>
+              <form
+                onSubmit={handleSubmit(onSubmit)}
+                className="space-y-4 md:space-y-6"
+                action="#"
+              >
+                <div>
+                  <label
+                    htmlFor="name"
+                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  >
+                    Your Name
+                  </label>
+                  <input
+                    type="name"
+                    name="name"
+                    {...register("name", { required: true })}
+                    className="bg-gray-50 border rounded-lg  block w-full p-2.5"
+                    placeholder="name"
+                  />
+                  {errors.name && (
+                    <span className="text-red-700">Name is required</span>
+                  )}
+                </div>
+                <div>
+                  <label
+                    htmlFor="email"
+                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  >
+                    your email
+                  </label>
+                  <input
+                    type="email"
+                    name="email"
+                    {...register("email", { required: true })}
+                    placeholder="name@company.com"
+                    className="bg-gray-50 border rounded-lg  block w-full p-2.5"
+                  />
+                  {errors.email && (
+                    <span className="text-red-700">Email is required</span>
+                  )}
+                </div>
+                <div>
+                  <label
+                    htmlFor="password"
+                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  >
+                    Password
+                  </label>
+                  <input
+                    type="password"
+                    name="password"
+                    {...register("password", {
+                      required: true,
+                      minLength: 6,
+                      maxLength: 20,
+                    })}
+                    placeholder="••••••••"
+                    className="bg-gray-50 border rounded-lg  block w-full p-2.5"
+                  />
+                  {errors.password?.type === "required" && (
+                    <span className="text-red-700">password is required</span>
+                  )}
+                  {errors.password?.type === "minLength" && (
+                    <span className="text-red-700">
+                      Password must be 6 characters
+                    </span>
+                  )}
+                  {errors.password?.type === "maxLength" && (
+                    <span className="text-red-700">
+                      Password must last 20 characters
+                    </span>
+                  )}
+                </div>
                 <input
-                  type="name"
-                  name="name"
-                  {...register("name", { required: true })}
-                  className="bg-gray-50 border rounded-lg  block w-full p-2.5"
-                  placeholder="name"
+                  type="submit"
+                  value="sign Up"
+                  className="bg-[#D1A054] p-3 rounded-lg mx-auto grid items-center"
                 />
-                {errors.name && <span className="text-red-700">Name is required</span>}
-              </div>
-              <div>
-                <label
-                  htmlFor="email"
-                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                >
-                  your email
-                </label>
-                <input
-                  type="email"
-                  name="email"
-                  {...register("email", { required: true })}
-                  placeholder="name@company.com"
-                  className="bg-gray-50 border rounded-lg  block w-full p-2.5"
-                />
-                 {errors.email && <span className="text-red-700">Email is required</span>}
-              </div>
-              <div>
-                <label
-                  htmlFor="password"
-                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                >
-                  Password
-                </label>
-                <input
-                  type="password"
-                  name="password"
-                  {...register("password", {required: true, minLength: 6, maxLength: 20})}
-                  placeholder="••••••••"
-                  className="bg-gray-50 border rounded-lg  block w-full p-2.5"
-                />
-                 {errors.name && <span className="text-red-700">password is required</span>}
-              </div>
-              <input
-                type="submit"
-                value="signUp"
-                className="bg-[#D1A054] p-3 rounded-lg mx-auto grid items-center"
-              />
-              <p className="text-sm font-light text-[#D1A054]">
-                New here?{" "}
-                <Link
-                  to="/login"
-                  className="font-semibold text-primary-600 hover:underline "
-                >
-                  Create a New Account
-                </Link>
-              </p>
-            </form>
+                <p className="text-sm font-light text-[#D1A054]">
+                  New here?{" "}
+                  <Link
+                    to="/login"
+                    className="font-semibold text-primary-600 hover:underline "
+                  >
+                    Create a New Account
+                  </Link>
+                </p>
+              </form>
+            </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </>
   );
 };
 
